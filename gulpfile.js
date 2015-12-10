c779458214e1784dc6ee33307d8e622d984b6a72
Arthur Shen <arthur@Arthurs-MBP.lan>
2015-12-10 11:25:16 +0800

    gulp coffee added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+
+
+// SRCS
+var coffeeSources = ['components/coffee/*.coffee']
 
 gulp.task('log',function(){
 	gutil.log('Work!');
+});
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
